c720c5b267f1036257fbe7ff6b838589972fe8e1
Shubham <ssp671995@gmail.com>
2022-03-29 19:43:24 +0530

    Added the welcome message.

---
+++ src/com/bridgelabz/HashTable.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+import java.util.Hashtable;
+
+public class HashTable {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the hash table program");
+    }
+}
